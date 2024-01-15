ff227da5244bfdecdbea9f5646c16e333178992e
Fiyinfoluwa Fajemirokun <fiyinfaj@gmail.com>
2024-01-15 17:15:29 -0500

    updated file

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "Jenkins-sg" {
 
 resource "aws_instance" "web" {
   ami                    = "ami-0005e0cfe09cc9050"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.large"
   key_name               = "prod"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
